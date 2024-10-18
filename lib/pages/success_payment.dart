6b70d741c84c969420c324846ac5048b9e87023c
testboiz <tvmini0202@gmail.com>
2024-10-18 15:42:01 +0700

    added success payment page

--- lib/pages/success_payment.dart
+++ lib/pages/success_payment.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tako_food/components/scaffold_components.dart';
 
 class PaymentSuccessPage extends StatefulWidget {
   const PaymentSuccessPage({super.key});
@@ -10,6 +11,34 @@ class PaymentSuccessPage extends StatefulWidget {
 class _PaymentSuccessPageState extends State<PaymentSuccessPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return PopScope(
+      child: Scaffold(
+        appBar: ScaffoldComponents.generateAppBar(context),
+        bottomNavigationBar: ScaffoldComponents.generateNavigationBar(context),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              children: [
+                const Text("Payment Successful"),
+                Image.asset("assets/img/check.png"),
+                const Text("Terima Kasih sudah memesan"),
+                const Text(
+                    "Harap menunggu panggilan untuk mengambil makanan Anda"),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.popUntil(
+                        context, ModalRoute.withName("/dashboard"));
+                  },
+                  child: const Text("Kembali ke Menu"),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+      onPopInvokedWithResult: (didpop, result) =>
+          Navigator.popUntil(context, ModalRoute.withName("/dashboard")),
+    );
   }
 }
