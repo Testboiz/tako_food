77a9a91f3c15f543360af273b6da098e67a03316
testboiz <tvmini0202@gmail.com>
2024-10-19 15:52:13 +0700

    added "profile" page

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -9,12 +9,48 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final List<String> list = <String>[
+    'Daan Mogot',
+    'Sunter',
+    'Mangga Besar',
+    'Mangga Dua'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: ScaffoldComponents.generateAppBar(context),
       bottomNavigationBar: ScaffoldComponents.generateNavigationBar(context),
-      body: const Placeholder(),
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Kode Referal"),
+            const TextField(),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Submit"),
+            ),
+            const Text("Pilih Cabang"),
+            DropdownButton<String>(
+              value: list.first,
+              onChanged: (String? value) {
+                setState(() {
+                  dropdownValue = value!;
+                });
+              },
+              items: list.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            )
+          ],
+        ),
+      )),
     );
   }
 }
--- lib/provider/auth_service.dart
+++ lib/provider/auth_service.dart
@@ -2,7 +2,6 @@ import 'dart:developer';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tako_food/provider/user_provider.dart';
