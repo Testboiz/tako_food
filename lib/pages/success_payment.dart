a3a29b654a9172838d467781201c357d55cf67fd
testboiz <tvmini0202@gmail.com>
2024-10-20 17:40:11 +0700

    success payment page complete

--- lib/pages/success_payment.dart
+++ lib/pages/success_payment.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tako_food/components/design_components.dart';
 import 'package:tako_food/components/scaffold_components.dart';
 
 class PaymentSuccessPage extends StatefulWidget {
@@ -18,18 +19,45 @@ class _PaymentSuccessPageState extends State<PaymentSuccessPage> {
         child: Padding(
           padding: const EdgeInsets.all(8),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text("Payment Successful"),
-              Image.asset("assets/img/check.png"),
-              const Text("Terima Kasih sudah memesan"),
               const Text(
-                  "Harap menunggu panggilan untuk mengambil makanan Anda"),
-              ElevatedButton(
+                "Payment Successful",
+                style: TextStyle(fontFamily: 'gotham', fontSize: 20),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: Image.asset("assets/img/check.png")),
+              const SizedBox(height: 20),
+              const Text(
+                "Terima Kasih sudah memesan",
+                style: TextStyle(fontFamily: 'gotham medium'),
+              ),
+              const Text(
+                "Harap menunggu panggilan untuk mengambil makanan Anda",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontFamily: 'gotham medium'),
+              ),
+              OutlinedButton(
                 onPressed: () {
                   Navigator.of(context)
                       .popUntil((route) => route.settings.name == '/dashboard');
                 },
-                child: const Text("Kembali ke Menu"),
+                style: OutlinedButton.styleFrom(
+                  side: BorderSide(
+                    width: 3.0,
+                    color: DesignComponents.gacoanPink,
+                  ),
+                ),
+                child: Text(
+                  "Kembali ke Menu",
+                  style: TextStyle(
+                    fontFamily: 'gotham medium',
+                    color: DesignComponents.gacoanPink,
+                  ),
+                ),
               )
             ],
           ),
