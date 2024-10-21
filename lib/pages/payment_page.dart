3c8ca4576a56c3189675cb612eef805a3eb1e015
testboiz <tvmini0202@gmail.com>
2024-10-21 17:14:08 +0700

    added options to pay in cash and pay in credit card

--- lib/pages/payment_page.dart
+++ lib/pages/payment_page.dart
@@ -53,27 +53,193 @@ class _PaymentPageState extends State<PaymentPage> {
               ),
               Align(
                 alignment: Alignment.bottomCenter,
-                child: SizedBox(
-                  width: double.infinity,
-                  child: OutlinedButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamed('/payment/success');
-                      cartProvider.buy();
-                    },
-                    style: OutlinedButton.styleFrom(
-                      side: BorderSide(
-                        width: 3.0,
-                        color: DesignComponents.gacoanPink,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    OutlinedButton(
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text("Informasi"),
+                              content: const Text(
+                                  "Silahkan ke kasir dan tunjukkan kode pemesanan PXYZS"),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context)
+                                        .popAndPushNamed('/payment/success');
+                                    cartProvider.buy();
+                                  },
+                                  child: const Text("Oke"),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                      style: OutlinedButton.styleFrom(
+                        side: BorderSide(
+                          width: 3.0,
+                          color: DesignComponents.gacoanPink,
+                        ),
+                      ),
+                      child: Text(
+                        "Bayar dengan Cash",
+                        style: TextStyle(
+                          fontFamily: 'gotham medium',
+                          color: DesignComponents.gacoanPink,
+                        ),
                       ),
                     ),
-                    child: Text(
-                      "Konfirmasi Pembayaran",
-                      style: TextStyle(
-                        fontFamily: 'gotham medium',
-                        color: DesignComponents.gacoanPink,
+                    OutlinedButton(
+                      onPressed: () {
+                        showModalBottomSheet(
+                          context: context,
+                          builder: (context) {
+                            return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: SizedBox(
+                                width: double.infinity,
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    const Text(
+                                      "Masukkan Info Kartu",
+                                      style: TextStyle(
+                                        fontFamily: 'gotham',
+                                        fontSize: 19,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 5),
+                                    TextField(
+                                      decoration: InputDecoration(
+                                        label: const Text("Card Number"),
+                                        prefixIcon:
+                                            const Icon(Icons.credit_card),
+                                        border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(30),
+                                        ),
+                                        contentPadding:
+                                            const EdgeInsets.symmetric(
+                                                horizontal: 20),
+                                        filled: true,
+                                        fillColor:
+                                            Colors.white.withOpacity(0.8),
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Row(
+                                      children: [
+                                        SizedBox(
+                                          width: 265,
+                                          child: TextField(
+                                            decoration: InputDecoration(
+                                              label: const Text("Expiry Date"),
+                                              prefixIcon: const Icon(
+                                                  Icons.calendar_today),
+                                              border: OutlineInputBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(30),
+                                              ),
+                                              contentPadding:
+                                                  const EdgeInsets.symmetric(
+                                                      horizontal: 20),
+                                              filled: true,
+                                              fillColor:
+                                                  Colors.white.withOpacity(0.8),
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          width: 130,
+                                          child: TextField(
+                                            decoration: InputDecoration(
+                                              label: const Text("CVC"),
+                                              prefixIcon:
+                                                  const Icon(Icons.numbers),
+                                              border: OutlineInputBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(30),
+                                              ),
+                                              contentPadding:
+                                                  const EdgeInsets.symmetric(
+                                                      horizontal: 20),
+                                              filled: true,
+                                              fillColor:
+                                                  Colors.white.withOpacity(0.8),
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                    const SizedBox(height: 10),
+                                    SizedBox(
+                                      width: double.infinity,
+                                      child: OutlinedButton(
+                                        onPressed: () {
+                                          Navigator.of(context)
+                                              .pushNamed('/payment/success');
+                                          cartProvider.buy();
+                                        },
+                                        style: OutlinedButton.styleFrom(
+                                          side: BorderSide(
+                                            width: 3.0,
+                                            color: DesignComponents.gacoanPink,
+                                          ),
+                                        ),
+                                        child: Text(
+                                          "Bayar",
+                                          style: TextStyle(
+                                            fontFamily: 'gotham medium',
+                                            color: DesignComponents.gacoanPink,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                      style: OutlinedButton.styleFrom(
+                        side: BorderSide(
+                          width: 3.0,
+                          color: DesignComponents.gacoanPink,
+                        ),
+                      ),
+                      child: Text(
+                        "Bayar dengan Kartu Kredit",
+                        style: TextStyle(
+                          fontFamily: 'gotham medium',
+                          color: DesignComponents.gacoanPink,
+                        ),
                       ),
                     ),
-                  ),
+                    OutlinedButton(
+                      onPressed: () {
+                        Navigator.of(context).pushNamed('/payment/success');
+                        cartProvider.buy();
+                      },
+                      style: OutlinedButton.styleFrom(
+                        side: BorderSide(
+                          width: 3.0,
+                          color: DesignComponents.gacoanPink,
+                        ),
+                      ),
+                      child: Text(
+                        "Konfirmasi Pembayaran dengan QRIS",
+                        style: TextStyle(
+                          fontFamily: 'gotham medium',
+                          color: DesignComponents.gacoanPink,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               )
             ],
